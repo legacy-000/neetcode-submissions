87bf67225f5597d9d4d337dab243574a32c30c3c
legacy-000 <cs2394204@gmail.com>
2026-06-20 22:02:44 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int first=0;
+        int last=nums.size()-1;
+        int mid=(first+last)/2;
+        while(first<=last){
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                first=mid+1;
+            }else{
+                last=mid-1;
+            }
+            mid=first+(last-first)/2;
+        }
+        return -1;
+    }
+};
